9e2ae0e01ee5ec05411b0633654f156e7f8e2b05
Jean Colombel <jeancolombel93@gmail.com>
2021-05-12 15:04:52 -0400

    Created 10 fake appartments

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,17 @@
 puts "Cleaning the database"
 Flat.destroy_all
 
-puts "Creating new flats"
-Flat.create!(
-  name: 'Light & Spacious Garden Flat London',
-  address: '10 Clifton Gardens London W9 1DT',
-  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
-  price_per_night: 75,
-  number_of_guests: 3
-)
+puts "Creating new flats..."
+
+10.times do
+  flat = Flat.create!(
+    name: "#{Faker::Ancient.god} suit",
+    address: Faker::Address.street_address,
+    description: Faker::Quote.matz,
+    price_per_night: rand(25..100),
+    number_of_guests: rand(1..5)
+  )
+  puts "Flat #{flat.id} was created."
+end
+
+puts "All done!"
